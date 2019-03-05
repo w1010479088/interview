8e5e1cfc7b25b7e724342fd2b3285d48ff3d6d05
mac <>
2019-03-05 10:36:48 +0800

    1.List的重构

--- src/com/lrlz/interview/DataHolder.java
+++ src/com/lrlz/interview/DataHolder.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /*
-* get()方法被经常使用,请问如何重构?
+* get()方法被经常使用,请问有何问题?如有问题,请重构.
 * */
 public class DataHolder {
     private List<Person> mDatas = new ArrayList<>();

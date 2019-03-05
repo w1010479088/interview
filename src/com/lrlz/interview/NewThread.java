6b28d869894324adadfaee15d7c003bad1b280c4
mac <>
2019-03-05 10:16:09 +0800

    1.interview

--- src/com/lrlz/interview/NewThread.java
+++ src/com/lrlz/interview/NewThread.java
@@ -3,7 +3,7 @@ package com.lrlz.interview;
 /*
 * 看到new Thread().start(),你想到了什么?
 * */
-public class ThreadEx {
+public class NewThread {
 
     private void createThread() {
         new Thread().start();

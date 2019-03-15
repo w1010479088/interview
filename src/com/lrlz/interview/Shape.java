aa34deceb8abfac20a7da961777763beb2fdf1bb
mac <>
2019-03-15 16:15:05 +0800

    1.小改动

--- src/com/lrlz/interview/Shape.java
+++ src/com/lrlz/interview/Shape.java
@@ -15,6 +15,10 @@ class Circle extends Shape {
     private Circle() {
         LogUtil.log("Circle()");
     }
+
+    public static void main(String[] args) {
+        new Circle();
+    }
 }
 
 class Pencil {
